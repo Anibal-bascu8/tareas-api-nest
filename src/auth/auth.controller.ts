6b5f8d3794ea39a85f0b45ee2a3691ea0f6be557
AnibalBascunan <ani.bascu8@gmail.com>
2024-07-22 18:14:13 -0400

    api lista con login jwt y validacion de roles de usuario

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,6 +4,11 @@ import { LoginUserDto } from './dto/login-user.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
+import { RoleProtected } from './decorators/role-protected.decorator';
+import { ValidRoles } from './interfaces/valid-roles';
+import { UserRoleGuard } from './guards/user-role.guard';
+import { GetUser } from './decorators/get-user.decorator';
+import { Auth } from './decorators/auth.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -31,4 +36,29 @@ export class AuthController {
     }
   }
 
+  @Get('private2')
+  @RoleProtected(ValidRoles.superUser)
+  @UseGuards( AuthGuard(), UserRoleGuard )
+  testingPrivateRout2(
+    @GetUser() user: User
+  ) {
+    
+    return {
+      ok: true,
+      message: 'hola mundo, entraste a una ruta privada'
+    }
+  }
+
+  
+  @Get('private3')
+  @Auth(ValidRoles.admin, ValidRoles.superUser)
+  testingPrivateRout3(
+    @GetUser() user: User
+  ) {
+    
+    return {
+      ok: true,
+      message: 'hola mundo, entraste a una ruta privada'
+    }
+  }
 }
